e5bc05d914d15d3c083b9f8654a71e8105a0fb0f
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-12-11 08:50:32 +0200

    Make Mama move when the player isn't looking

--- src/main/kotlin/com/flooferland/waygetter/systems/EntitySight.kt
+++ src/main/kotlin/com/flooferland/waygetter/systems/EntitySight.kt
@@ -10,17 +10,31 @@ import net.minecraft.world.entity.*
  * Uses [EntityLine] internally.
  */
 class EntitySight(private val owner: Entity, val maxDist: Double) {
+    val maxDistSqrt = maxDist * maxDist
+
     private val line = EntityLine(owner, maxDist)
 
     fun firstSeen(): ServerPlayer? {
-        return line.getFirst()
+        val level = owner.level() as? ServerLevel ?: return null
+        for (player in level.players()) {
+            if (player.distanceToSqr(owner) > maxDistSqrt) continue
+            if (seenBy(player)) return player
+        }
+        return null
     }
 
-    fun seenByAnyone(): Boolean {
-        return line.hasLineToAny()
-    }
+    fun seenByAnyone() = firstSeen() != null
 
+    /** Directly raycasts to the player and checks if the entity is in view */
     fun seenBy(player: ServerPlayer): Boolean {
-        return line.hasLineTo(player)
+        if (!line.hasLineTo(player)) return false
+
+        val entityViewPos = owner.boundingBox.center
+        val playerViewPos = player.eyePosition
+        val playerLook = player.lookAngle
+
+        val toSelf = entityViewPos.subtract(playerViewPos).normalize()
+        val dot = playerLook.dot(toSelf)
+        return dot > 0f
     }
 }
